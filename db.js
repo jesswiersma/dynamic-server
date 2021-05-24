65d57e1c1c6a9190c19bbb5edd7d53c8a8842566
Jessica Wiersma <jesswiersma@gmail.com>
2021-05-24 10:13:40 -0400

    updated db.js

--- db.js
+++ db.js
@@ -2,7 +2,13 @@ const Sequelize = require("sequelize").Sequelize;
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    ssl: process.env.ENVIRONMENT === "production"
+    dialetOptions: !process.env.DATABASE_URL.includes('localhost') ? {
+    ssl:{
+        require: true,
+        rejectedUnauthorized: false,
+        //process.env.ENVIRONMENT === "production"
+    }
+} : {} 
 });
 
 module.exports = sequelize;
